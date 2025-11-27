b53f3ba1e0f898f378b97c7f285f192ff4da2d89
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2025-11-27 16:01:12 +0800

    update

--- test/human_detection/alarm.py
+++
@@ -1,9 +0,0 @@
-import simpleaudio as sa
-import threading
-
-class AlarmManager:
-    def __init__(self, sound_file):
-        self.sound = sa.WaveObject.from_wave_file(sound_file)
-    
-    def play_alarm(self):
-        threading.Thread(target=self.sound.play).start()
